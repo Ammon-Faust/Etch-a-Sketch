780e301d16c69f96766df9ea1c8ea2fa1a3ce517
Ammon Faust <119088185+Ammon-Faust@users.noreply.github.com>
2023-12-26 18:20:22 -0700

    add: slider to adjust how many cells/pixels in grid

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 const grid = document.getElementById("grid");
-const reset = document.getElementById("reset")
-
-
+const reset = document.getElementById("reset");
+const slider = document.getElementById("slider");
 
 function makeGrid(rows, cols) {
+  
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+
+    const cellWidth = 100 / cols;
+    const cellHeight = 100 / rows;
+
     for (let i = 0; i < rows * cols; i++) {
         const cell = document.createElement('div');
         cell.classList.add('cells');
+        cell.style.width = `calc(${cellWidth}% - 0px)`; 
+        cell.style.height = `calc(${cellHeight}% - 0px)`; 
 
-        // Add hover effect using JavaScript
         cell.addEventListener('mouseover', function() {
             this.classList.add('filled');
         });
@@ -19,15 +27,19 @@ function makeGrid(rows, cols) {
 
 makeGrid(16, 16);
 
-
 function gridReset() {
     reset.addEventListener("click", function() {
         const filledCells = grid.querySelectorAll('.filled');
         filledCells.forEach(cell => {
             cell.classList.remove('filled');
-            cell.style.backgroundColor = ''; // Reset background color
+            cell.style.backgroundColor = ''; 
         });
     });
 }
 
 gridReset();
+
+slider.addEventListener('input', function() {
+    const gridSize = parseInt(this.value); 
+    makeGrid(gridSize, gridSize);
+});
